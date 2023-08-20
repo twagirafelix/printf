e384cda3459cac69d11639a91c45434c3aa7d2b8
Kris23k <rugambwachriskevin@gmail.com>
2023-08-21 04:49:52 +1200

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,6 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -32,13 +31,8 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
