00ffd74bd19e26deb66fcd22a396f4e7045ad2b3
Kris23k <rugambwachriskevin@gmail.com>
2023-08-21 04:04:00 +1200

    printf.c

--- printf.c
+++ printf.c
@@ -1,66 +1,80 @@
-#include "main.h"
+# include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+int parser(const char *format, fmt_spec f_list[], va_list args);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - function that prints characters
+ *@format: format specifier
+ *
+ * Return: Always 0
  */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	int printed_char = 0;
+	va_list args;
+	fmt_spec formats[] = {
+		{"c", handle_char},
+		{"s", handle_string},
+		{"%", handle_percentage},
+		{"d", handle_decimals},
+		{"i", handle_decimals},
+		{"b", binary_function},
+		{"o", octal_function},
+		{"x", hexa_function},
+		{"X", heXa_function},
+		{"u", handle_unsigned_int},
+		{"S", handle_S_string},
+		{NULL, NULL}
+	};
+	va_start(args, format);
+	printed_char = parser(format, formats, args);
+	va_end(args);
+	return (printed_char);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+ * parser - A format replacement function
+ * @format: The format to be considered
+ * @formats: The list of available formats
+ * @args: The arguments to be passed
+ * Return: The number of arguments printed
+*/
 
-	*buff_ind = 0;
+int parser(const char *format, fmt_spec formats[], va_list args)
+{
+	int i = 0, j, printed_char = 0;
+	char fmt_spec = '%';
+
+	if (format == NULL)
+		format = "";
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		j = 0;
+		if (format[i] == fmt_spec)
+		{
+			while (formats[j].fmt != NULL)
+			{
+				if (format[i + 1] == formats[j].fmt[0])
+				{
+					printed_char += formats[j].function(args);
+					i++;
+					break;
+				}
+				j++;
+			}
+			if (formats[j].fmt == NULL && format[i + 1] != '\0')
+			{
+				_putchar(format[i]);
+				printed_char++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed_char++;
+		}
+	}
+	return (printed_char);
 }
